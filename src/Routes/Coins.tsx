98d27b7cbc5f468cc2180148ebf188cb0b58338c
unknown <crsn1111@gmail.com>
2021-12-24 16:51:36 +0900

    hash

--- src/Routes/Coins.tsx
+++ src/Routes/Coins.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { json } from "stream/consumers";
 import styled from "styled-components";
 
 const Container = styled.div`
--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Coin from "./Coin";
 import Coins from "./Coins";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId" element={<Coin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default Router;
