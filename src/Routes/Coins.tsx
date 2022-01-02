703569d5adccfb849a6273270edcf0c1babbeb03
unknown <crsn1111@gmail.com>
2022-01-02 16:55:14 +0900

    touchmove

--- src/Routes/Coins.tsx
+++ src/Routes/Coins.tsx
@@ -102,8 +102,10 @@ const Coins = () => {
   };
   useEffect((): ReturnType<EffectCallback> => {
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("touchmove", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("touchmove", handleScroll);
     };
   });
   return (
