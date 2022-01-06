c2305d2de78bc28dca6119f25c7d03856c65cae3
unknown <crsn1111@gmail.com>
2022-01-06 17:51:40 +0900

    hashrouter

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Coin from "../Components/Coin";
 import Coins from "../Components/Coins";
 import Header from "./Header";
 
 const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Header/>
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId/*" element={<Coin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default Router;
