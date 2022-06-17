79b0cef6d6fce8fe791b8f90b7e3fdb515301602
김지호 <crsn1111@gmail.com>
2022-06-17 22:32:41 +0900

    [style] reset del

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
 ${reset}
   * {
     box-sizing: border-box;
